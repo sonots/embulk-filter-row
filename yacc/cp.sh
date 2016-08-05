2105e931781d737600e317069154b6cdbfbcc689
sonots <sonots@gmail.com>
2016-08-06 04:08:59 +0900

    remove unnecessary files

--- yacc/cp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp where.y where.flex ../src/main/java/org/embulk/filter/row/where
-sed 's/\/\/package/package/' ParserNode.java  > ../src/main/java/org/embulk/filter/row/where/ParserNode.java
-
