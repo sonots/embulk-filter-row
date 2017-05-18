f635dece85da35d1025180433c58383d11fa3ee4
sonots <sonots@gmail.com>
2017-05-18 11:55:04 +0900

    Use joni to improve REGEXP performance

--- src/main/java/org/embulk/filter/row/where/ParserExp.java
+++ src/main/java/org/embulk/filter/row/where/ParserExp.java
@@ -4,8 +4,10 @@ import org.embulk.config.ConfigException;
 import org.embulk.spi.PageReader;
 import org.embulk.spi.time.Timestamp;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.jcodings.specific.UTF8Encoding;
+import org.joni.Matcher;
+import org.joni.Option;
+import org.joni.Regex;
 
 // Operation Node of AST (Abstract Syntax Tree)
 public abstract class ParserExp extends ParserNode
@@ -282,12 +284,15 @@ class StringOpExp extends BinaryOpExp
 
 class RegexpOpExp extends BinaryOpExp
 {
-    Pattern pattern;
+    Regex regex;
 
     public RegexpOpExp(ParserLiteral left, ParserLiteral right, int operator)
     {
         super(left, right, operator);
-        this.pattern = Pattern.compile(((StringLiteral)right).val);
+
+        byte[] pattern = (((StringLiteral)right).val).getBytes();
+        this.regex = new Regex(pattern, 0, pattern.length, Option.NONE, UTF8Encoding.INSTANCE);
+
         if (! left.isString()) {
             throw new ConfigException(String.format("\"%s\" is not a String column", ((IdentifierLiteral)left).name));
         }
@@ -300,9 +305,10 @@ class RegexpOpExp extends BinaryOpExp
 
     public boolean eval(PageReader pageReader)
     {
-        String l = left.getString(pageReader);
-        Matcher m = pattern.matcher(l);
-        return m.find();
+        byte[] l = left.getString(pageReader).getBytes();
+        Matcher matcher = regex.matcher(l);
+        int result = matcher.search(0, l.length, Option.DEFAULT);
+        return result != -1;
     }
 }
 
